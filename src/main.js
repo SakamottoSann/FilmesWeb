2b65c404707763e8b6543f5705456e2b78592c13
Willian <willan.dafonseca@hotmail.com>
2021-10-01 21:44:03 -0300

    :sparkles: Server Project

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+
+Vue.config.productionTip = false
+Vue.prototype.$urlAPI = "http://localhost:3000"
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
